7a8060ee36f1197290629f8e03f0f3d8469e7af0
Deep Patel <deep.ag123007@gmail.com>
2019-07-16 14:32:02 -0500

    linq million exercise

---
+++ Bank.cs
@@ -0,0 +1,9 @@
+namespace LINQ_Millions
+{
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+
+}
---
+++ ReportItem.cs
@@ -0,0 +1,9 @@
+namespace LINQ_Millions
+{
+    public class ReportItem
+    {
+        public string CustomerName { get; set; }
+        public string BankName { get; set; }
+    }
+
+}
